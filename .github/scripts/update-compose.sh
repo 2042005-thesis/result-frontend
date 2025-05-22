a93ed06b26c58b0510df855cdd27f4c941eba843
Taufiq Permana <35789663+taufiqpsumarna@users.noreply.github.com>
2025-05-22 09:37:08 +0000

    Add git remote url

--- .github/scripts/update-compose.sh
+++ .github/scripts/update-compose.sh
@@ -2,18 +2,24 @@
 set -e  # Exit on error
 
 # Determine the correct docker-compose file to update
+if [[ "${GITHUB_REF}" == "refs/heads/dev" ]]; then
+  COMPOSE_FILE="apps/dev/docker-compose.yml"
+  BRANCH="dev"
+  ALLOW_UPDATE=true  # Always allow updates for dev (Git SHA)
+
 if [[ "${GITHUB_REF}" == "refs/heads/staging" ]]; then
-  COMPOSE_FILE="env/staging/docker-compose.yml"
+  COMPOSE_FILE="apps/staging/docker-compose.yml"
   BRANCH="staging"
-  ALLOW_UPDATE=true  # Always allow updates for staging (Git SHA)
 
 elif [[ "${GITHUB_REF}" == refs/tags/v* ]]; then
   # Extract the version from the tag (e.g., v1.2.3)
+  BRANCH="main"
   VERSION_TAG="${GITHUB_REF#refs/tags/}"
+  ALLOW_UPDATE=true
 
   # Allow update only if it's a stable semantic version (vX.Y.Z)
   if [[ "$VERSION_TAG" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    COMPOSE_FILE="env/production/docker-compose.yml"
+    COMPOSE_FILE="apps/production/docker-compose.yml"
     BRANCH="main"
     ALLOW_UPDATE=true
   else
@@ -22,9 +28,11 @@ elif [[ "${GITHUB_REF}" == refs/tags/v* ]]; then
   fi
 
   # Ensure we are on the main branch
-  git fetch origin main  # Fetch latest main branch
-  git checkout main || git checkout -b main origin/main  # Switch to main branch
-  git reset --hard origin/main  # Ensure branch is up to date
+  # Clonning deployment repository
+  git clone $DEPLOYMET_REPO
+  git fetch origin dev  # Fetch latest main branch
+  git checkout dev || git checkout -b dev origin/dev  # Switch to main branch
+  git reset --hard origin/dev  # Ensure branch is up to date
 
 else
   echo "Branch not recognized, skipping update."
@@ -38,8 +46,9 @@ if [[ "$ALLOW_UPDATE" == "true" ]]; then
   git config user.email "github-actions[bot]@users.noreply.github.com"
 
   # Update docker image tag in docker-compose.yml
+  # Matching
   echo "Updating $COMPOSE_FILE with image: $DOCKER_METADATA_OUTPUT_VERSION"
-  sed -E -i "s|(image:\s*)[^ ]+|\1$DOCKER_METADATA_OUTPUT_TAGS|" "$COMPOSE_FILE"
+  sed -E -i "s|(image: $DEPLOYMENT_IMAGE_NAME\s*)[^ ]+|\1$DOCKER_METADATA_OUTPUT_TAGS|" "$COMPOSE_FILE"
 
   # Stage changes
   git add "$COMPOSE_FILE"
